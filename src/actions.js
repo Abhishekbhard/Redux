be1b6d2aaa619013db28318ff6d20ec7b71fa4fc
Abhishek <eduandabhi@gmail.com>
2021-09-08 21:27:27 +0530

    Custom Store

---
+++ src/actions.js
@@ -0,0 +1,18 @@
+import { BUG_ADDED, BUG_RESOLVED } from "./actionTypes";
+
+export function bugAdded(description) {
+  return {
+    type: BUG_ADDED,
+    payload: {
+      description: description,
+    },
+  };
+}
+export function bugResolved(id) {
+  return {
+    type: BUG_RESOLVED,
+    payload: {
+      id,
+    },
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
-import { compose, pipe } from "lodash/fp";
-
-const tag = ({ tag }) => tag;
-
-//console.log(tag);
-
-const toLowerCase = (str) => str.toLowerCase();
-const addParanthesis =
-  ({ opening, closing }) =>
-  (toLowerCase) =>
-    opening.concat(toLowerCase).concat(closing);
-// opening.concat(str).concat(closing).toString();
-
-const transform = pipe(
-  tag,
-  toLowerCase,
-  addParanthesis({ opening: "(", closing: ")" })
-);
-const input = { tag: "JAVASCRIPT" };
-
-const output = transform(input);
-console.log(output);
+import store from "./customStore";
+import { bugAdded, bugResolved } from "./actions";
+store.subscribe(() => {
+  console.log("Store");
+});
+store.dispatch(bugAdded("bug1"));
+store.dispatch(bugResolved(1));
+console.log(store.getState());
+//console.log(store.getState());
