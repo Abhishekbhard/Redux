304af3c876d5d35c9c5fecf52ee225cfbe5e4150
Abhishek <eduandabhi@gmail.com>
2021-09-13 19:01:13 +0530

    parameterize middleware

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,6 @@ import reducer from "./reducer";
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "../middleware/logger";
 
-const store = configureStore({ reducer, middleware: [logger] });
+const store = configureStore({ reducer, middleware: [logger("console")] });
 
 export default store;
