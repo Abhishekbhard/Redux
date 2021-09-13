62acf8251c2b3b53c46beaaf005e8c9ed031f93d
Abhishek <eduandabhi@gmail.com>
2021-09-13 18:56:26 +0530

    middleware function

--- src/index.js
+++ src/index.js
@@ -8,17 +8,17 @@ import {
   getUnresolvedBugs,
 } from "./store/bugs";
 import { userAdded } from "./store/users";
-store.subscribe(() => {
-  console.log("Store changed");
-});
+// store.subscribe(() => {
+//   console.log("Store changed");
+// });
 store.dispatch(userAdded({ name: "User 1" }));
-store.dispatch(userAdded({ name: "User 2" }));
-store.dispatch(projectAdded({ name: "project 1" }));
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugResolved({ id: 2 }));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-store.dispatch(bugAssignedToUser({ bugId: 2, userId: 1 }));
+// store.dispatch(userAdded({ name: "User 2" }));
+// store.dispatch(projectAdded({ name: "project 1" }));
+// store.dispatch(bugAdded({ description: "Bug 1" }));
+// store.dispatch(bugAdded({ description: "Bug 2" }));
+// store.dispatch(bugResolved({ id: 2 }));
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
+// store.dispatch(bugAssignedToUser({ bugId: 2, userId: 1 }));
 
 const getUnresolvedBug = getUnresolvedBugs(store.getState());
 const bugs = getBugsByUser(2)(store.getState());
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import reducer from "./reducer";
 import { configureStore } from "@reduxjs/toolkit";
+import logger from "../middleware/logger";
 
-const store = configureStore({ reducer });
+const store = configureStore({ reducer, middleware: [logger] });
 
 export default store;
