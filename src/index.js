1613ae7b5a95899d1493636ec86d830c34e6a913
Abhishek <eduandabhi@gmail.com>
2021-09-22 14:57:13 +0530

    Getting Data from Server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import store from "./store/store";
 import * as actions from "./store/api";
+import { loadBugs } from "./store/bugs";
 
-store.dispatch(
-  actions.apiCallBegan({
-    url: "/bugs",
-    data: {},
-    onSuccess: "bugReceived",
-  })
-);
+store.dispatch(loadBugs());
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,34 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { createSelector } from "reselect";
+import { apiCallBegan } from "./api";
 
 //reducer
 let lastId = 0;
 
 const slice = createSlice({
   name: "bugs",
-  initialState: [],
+  initialState: {
+    list: [],
+    loading: false,
+    lastFetch: null,
+  },
   reducers: {
+    bugsReceived: (bugs, action) => {
+      bugs.list = action.payload;
+    },
     bugAssignedToUser: (bugs, action) => {
       const { bugId, userId } = action.payload;
-      const index = bugs.findIndex((bug) => bug.id === bugId);
-      bugs[index].userId = userId;
+      const index = bugs.list.findIndex((bug) => bug.id === bugId);
+      bugs.list[index].userId = userId;
     },
     bugAdded: (bugs, action) => {
-      bugs.push({
+      bugs.list.push({
         id: ++lastId,
         description: action.payload.description,
         resolved: false,
       });
     },
     bugResolved: (bugs, action) => {
-      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-      bugs[index].resolved = true;
+      const index = bugs.list.findIndex((bug) => bug.id === action.payload.id);
+      bugs.list[index].resolved = true;
     },
   },
 });
 
-export const { bugAdded, bugResolved, bugAssignedToUser } = slice.actions;
+export const { bugAdded, bugResolved, bugAssignedToUser, bugsReceived } =
+  slice.actions;
 export default slice.reducer;
+
+//Action Creattors
+
+const url = "/bugs";
+
+export const loadBugs = () =>
+  apiCallBegan({
+    url,
+    data: {},
+    onSuccess: bugsReceived.type,
+  });
 
 //selecters
 
